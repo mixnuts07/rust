8a833255429af09dace470a7563bba7da8708e3e
mixnuts07 <french0719@gmail.com>
2023-08-22 14:58:18 +0900

    sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+# コンパイル
+rustc main.rs
+# プロジェクトの作成
+cargo new project_name
+# ビルド＆実行
+cargo build
+./src/main.rs
+# ビルド＆実行
+cargo run
+# コンパイル可能かチェック
+cargo check
+# リリースに向けたビルド
+cargo build --release
