efbbf9abc23fa70586da7acb34de2c905cd1be97
mixnuts07 <french0719@gmail.com>
2023-08-25 17:19:26 +0900

    add own

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 fn main() {
     let mut x: i32 = -5;
     println!("The value Of x is: {}", x); // -5
@@ -34,6 +36,14 @@ fn main() {
     println!("{}", func_return);
     println!("{}", plus_one(5));
     if_state(4);
+    let condition = true;
+    let num_if = if condition { 5 } else { 6 };
+    println!("num_if is {}", num_if);
+    loop_st();
+    while_st();
+    for_st();
+    for_array_st();
+    own();
 }
 
 fn another_function(x: i32) -> i32 {
@@ -50,9 +60,72 @@ fn plus_one (x: i32) -> i32 {
 }
 
 fn if_state(number: usize) {
-    if number < 5 {
-        println!("Conditions was true");
+    if number > 5  {
+        println!("number was over 5");
+    } else if number % 5 == 0 {
+        println!("number was True");
     } else {
-        println!("Conditions was false");
+        println!("number was false");
     }
+}
+
+fn loop_st() {
+    let mut count: usize = 0;
+    'counting_up: loop {
+        println!("LOOP");
+        
+        let mut remaining = 10;
+        
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+}
+
+fn while_st() {
+    let mut number: usize = 3;
+
+    while number != 0  {
+        println!("{}", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!");
+}
+
+fn for_st() {
+    let a: [i32; 5] = [10, 20, 30, 40, 50];
+    let mut index: usize = 0;
+    while index < 5 {
+        println!("the value is {}", a[index]);
+        index += 1;
+    }
+}
+
+fn for_array_st() {
+    let a: [i32; 5] = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is {}", element);
+    }
+}
+// ownership
+// Each value in Rust has an owner.
+// There can only be one owner at a time.
+// When the owner goes out of scope, the value will be dropped.
+fn own() {
+    let mut s: String = String::from("Hello");
+    s.push_str(", Ownership");
+
+    println!("{}", s);
+
 }
