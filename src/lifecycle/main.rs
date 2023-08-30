933f8bb8b8e9a838d24af7fc768ed4610514c559
mixnuts07 <french0719@gmail.com>
2023-08-30 09:08:26 +0900

    add life time

---
+++ src/lifecycle/main.rs
@@ -0,0 +1,26 @@
+use std::fmt::Display;
+
+// life time <'T> (この場合だと、この関数を使用する先でxかyの短い方のスコープのライフサイクルに合わせられる)
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
