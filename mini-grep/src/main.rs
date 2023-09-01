22536750858ffa208d38d22133f77dd4ce628031
mixnuts07 <french0719@gmail.com>
2023-09-01 23:58:24 +0900

    mini kube

--- mini-grep/src/main.rs
+++ mini-grep/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,4 +8,10 @@ fn main() {
     let filename: &String = &args[2];
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("File Not Found");
+
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents).expect("something went wrong reading the file.");
 }
