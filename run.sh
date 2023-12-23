6037dab4a45eba72a8e73e560e6b78e96751f254
mixnuts07 <french0719@gmail.com>
2023-12-23 23:31:45 +0900

    wip 2

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
-# コンパイル
+# コンパイルによってバイナリの実行可能ファイルを出力
 rustc main.rs
+# 実行
+./main
+
 # プロジェクトの作成
 cargo new project_name
 # ビルド＆実行
@@ -9,7 +12,7 @@ cargo build
 cargo run
 # コンパイル可能かチェック
 cargo check
-# リリースに向けたビルド
+# リリースに向けたビルド（最適化された状態でコンパイルできる）
 cargo build --release
 # テストの実行 （#[test]がついているもの）
 cargo test
