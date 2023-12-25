76d7839e84b0e2b3dacc0b2fb06159e9e42c97e1
mixnuts07 <french0719@gmail.com>
2023-12-25 21:59:33 +0900

    done struct

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -20,6 +20,20 @@ fn main() {
     takes_ownership(s); // sの値が関数にムーブされた
     let x = 5; // xがスコープに入る
     makes_copy(x); // xも関数にムーブされるが、i32はCopyなので、この後にxを使っても大丈夫
+
+    let mut user1 = User {
+        email: String::from("someone@gmail.com"),
+        username: String::from("hoge"),
+        active: true,
+        sign_in_count: 1
+    };
+    user1.email = String.from("another@gmail.com"); // インスタンス(user1)がmutableなので、フィールドの値を変更できる
+    // インスタンス全体が可変でなければならないことに注意。一部のフィールドのみを可変にすることはできない。
+    let black = Color(0, 0, 0); // タプル構造体
+    black.0; // 0
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    println!("The area of the rectangle is {:#?} square pixels.", rect1.area());
 }
 
 fn takes_ownership(some_thing: String) {
@@ -28,3 +42,31 @@ fn takes_ownership(some_thing: String) {
 fn makes_copy(some_integer: i32) {
     println!("{}", some_integer);
 } // ここでsome_integerがスコープを抜ける。何も特別なことはない。
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1
+    }
+}
+struct Color(i32, i32, i32);
+
+#[derive(Debug)] // デバッグ出力を可能にする
+struct Rectangle {
+    width: u32,
+    height: u32,
+
+}
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
