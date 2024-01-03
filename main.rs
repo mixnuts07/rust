c2357bd97c7e02d979ce5cf59d3077b59f616adf
mixnuts07 <french0719@gmail.com>
2024-01-03 23:59:35 +0900

    done 9

--- main.rs
+++ main.rs
@@ -1,6 +1,17 @@
+use std::fs::File;
+
 fn main() {
     let x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    let f = File::open("hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("There was a problem opening the file: {:?}", error)
+        }
+    };
 }
+
